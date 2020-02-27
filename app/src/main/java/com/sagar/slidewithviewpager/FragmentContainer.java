debf8f8303425757409fba9514ff3873dc88bd6d
omgkali <looksagar59@gmail.com>
2020-02-27 15:55:30 +0600

    first commit

---
+++ app/src/main/java/com/sagar/slidewithviewpager/FragmentContainer.java
@@ -0,0 +1,21 @@
+package com.sagar.slidewithviewpager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentContainer extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+
+        return rootView;
+    }
+}
